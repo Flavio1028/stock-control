18c5e85cae0e97bc9b2eb4d6be5b89c0262a69a5
Flávio Rocha <flavio-10_28@hotmail.com>
2024-12-20 10:36:30 -0300

    Criação da funcionalidade excluir categoria

--- src/app/modules/categories/page/categories-home/categories-home.component.ts
+++ src/app/modules/categories/page/categories-home/categories-home.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 import { Subject, takeUntil } from 'rxjs';
+import { DeleteCategoryAction } from 'src/app/models/interfaces/categories/event/DeleteCategoryAction';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 
@@ -49,6 +50,43 @@ export class CategoriesHomeComponent implements OnInit, OnDestroy {
         }
       });
 
+  }
+
+  handleDeleteCategoryAction(event: DeleteCategoryAction): void {
+    if (event) {
+      this.confirmationService.confirm({
+        message: `Confirma a exclusão da categoria: ${event?.categoryName}`,
+        header: 'Confirmação de exclusão',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => this.deleteCategory(event?.category_id)
+      });
+    }
+  }
+
+  deleteCategory(category_id: string): void {
+    this.categoriesService.deleteCategory({ category_id })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Sucesso',
+            detail: 'Categoria removida com sucesso!',
+            life: 2500
+          });
+          this.getAllCategories();
+        }, error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao remover categorias!',
+            life: 2500
+          });
+        }
+      });
   }
 
   ngOnDestroy(): void {
