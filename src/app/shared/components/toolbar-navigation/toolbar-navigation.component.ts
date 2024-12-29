1be14dddbf790aa9be494b4dbc2a504523e16497
Flavio Rocha <flavio-10_28@hotmail.com>
2024-12-29 10:25:20 -0300

    Criação da funcionalidade vender produto

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { DialogService } from 'primeng/dynamicdialog';
+import { ProductEvent } from 'src/app/models/enums/products/ProductEvents';
+import { ProductFormComponent } from 'src/app/modules/products/components/product-form/product-form.component';
 
 @Component({
   selector: 'app-toolbar-navigation',
@@ -11,12 +14,26 @@ export class ToolbarNavigationComponent {
 
   constructor(
     private cookieService: CookieService,
-    private router: Router
+    private router: Router,
+    private dialogService: DialogService
   ) { }
 
   handleLogout(): void {
     this.cookieService.delete('USER_INFO');
     void this.router.navigate(['/home']);
+  }
+
+  handleSaleProduct(): void {
+    this.dialogService.open(ProductFormComponent, {
+      header: ProductEvent.SALE_PRODUCT_EVENT,
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true,
+      data: {
+        event: { action: ProductEvent.SALE_PRODUCT_EVENT }
+      }
+    });
   }
 
 }
